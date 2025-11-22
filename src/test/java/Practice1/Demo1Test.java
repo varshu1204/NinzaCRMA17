a984839e4cf43ce78f863b0b55c1f703154b58f8
QSP <QSP@DESKTOP-K5OA249>
2025-11-22 15:19:16 +0530

    sdfgh

--- src/test/java/Practice1/Demo1Test.java
+++ src/test/java/Practice1/Demo1Test.java
@@ -1,19 +1,19 @@
 package Practice1;
 
 import org.testng.Reporter;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class Demo1Test {
 
 	
 	@Test(groups = "smoke")
-	public void appleTest()
-	
+	public void AppleTest()
 	{
 		Reporter.log("Apple",true);
-		Reporter.log("Grapes",true);
+		Reporter.log("Apple",true);
+
 	}
-	
 	
 	
 	
